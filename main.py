b8809d3df76fbc1bf16914de2ee7cf2296cd7d63
Rohit Gupta <88101190+rohitx26@users.noreply.github.com>
2025-05-04 03:04:34 +0530

    added endoints

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+
 def main():
     print("Hello from pythonuvdemo!")
+    uvicorn.run("main:app", host="0.0.0.0", port=8001, reload=True)
 
 
 if __name__ == "__main__":
     main()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/users")
+def read_users():
+    return {"users": ["alice", "bob", "charlie"]}
